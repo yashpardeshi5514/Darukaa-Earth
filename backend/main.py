d3030813e318f8bf3327494a7a1dd5c4619f3b81
Yash Pardeshi <yashpardeshi9545@gmail.com>
2026-09-18 16:23:51 +0530

    Allow production frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://darukaa-earth-delta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
